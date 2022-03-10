243acf57a8c5d0e9a4465295b1f0d69cb6d68f21
Moiseicho 45 <mateizahariev@gmail.com>
2022-03-11 01:47:24 +0200

    Adding the actual character selection menu changes. False commit due to unaddable unity temporary files.

---
+++ Spaceman Hacktues/Assets/scripts/GameMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameMenu : MonoBehaviour
+{
+    //Text
+    //public Text HPTxt, HPMaxTxt, levelTxt, monsTxt, EXPTxt, upcostTxt;
+
+    //Logic
+    private int characterNum;
+    public Image characterSprite;
+    public void OnArrowClick(bool right)
+    {
+        if(right)
+        {
+            characterNum++;
+            if(characterNum == gamemanager.instance.playersprites.Count)
+            {
+                characterNum = 0;
+            }
+            changeCharacter();
+        }
+        else
+        {
+            characterNum--;
+            if(characterNum<0)
+            {
+                characterNum = gamemanager.instance.playersprites.Count-1;
+            }
+            changeCharacter();
+        }
+    } 
+    private void changeCharacter()
+    {
+        characterSprite.sprite = gamemanager.instance.playersprites[characterNum];
+        gamemanager.instance.player.swapsprite(characterNum);
+    }
+    public void updateMenu()
+    {
+        
+    }
+}
