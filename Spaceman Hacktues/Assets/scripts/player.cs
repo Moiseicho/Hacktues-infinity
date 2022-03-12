e0edcdf7d295ed81b17c0d39869596ac72bf17f7
Moiseicho 45 <mateizahariev@gmail.com>
2022-03-12 16:39:40 +0200

    Restored the old backup file due to bad merge

--- Spaceman Hacktues/Assets/scripts/player.cs
+++ Spaceman Hacktues/Assets/scripts/player.cs
@@ -6,15 +6,12 @@ public class player : AImovement
 {   
     public SpriteRenderer sprite;
     public int PlayerLevel;
-    public Animator animator;
-    
-    //Vector2 movement;
 
     protected override void Start()
     {
         base.Start();
         sprite = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
+
         swapsprite();
 
         DontDestroyOnLoad(gameObject);
@@ -24,21 +21,8 @@ public class player : AImovement
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         // x and y are -1/0/1 depending on which direction button you are pressing
-        if(x == 0 && y == 0){
-            animator.SetFloat("Speed", 0);
-        }
-        else{
-            animator.SetFloat("Speed", 1);
-            animator.SetFloat("Horizontal", x);
-            animator.SetFloat("Vertical", y);
-        }
-        UpdateMotor(new Vector3(x, y, 0));
-        //animator.SetFloat("Horizontal", movement.x);
-        //animator.SetFloat("Vertical", movement.y);
-        //animator.SetFloat("Speed", movement.sqrMagnitude);
 
-        
-        
+        UpdateMotor(new Vector3(x, y, 0));
     }
     public void swapsprite()
     {
--- Spaceman Hacktues/Assets/scripts/teleport.cs
+++ Spaceman Hacktues/Assets/scripts/teleport.cs
@@ -6,21 +6,12 @@ using UnityEngine.SceneManagement;
 public class teleport : cancolide
 {
     public string level;
-    public Animator transition;
-    public float waittime;
     protected override void Oncollide(Collider2D coll)
     {
         if(coll.name == "player")
         {
-            StartCoroutine(WaitForTrans());
+            gamemanager.instance.SaveState();
+            SceneManager.LoadScene(level);
         }
-    }
-    IEnumerator WaitForTrans()
-    {
-        transition.SetTrigger("Start");
-        yield return new WaitForSeconds(waittime);
-
-        gamemanager.instance.SaveState();
-        SceneManager.LoadScene(level);
     }
 }
