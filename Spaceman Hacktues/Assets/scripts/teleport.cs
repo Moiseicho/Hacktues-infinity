861e646d92eee1aacd739c52a556b93f5ecd8b35
Moiseicho 45 <mateizahariev@gmail.com>
2022-03-12 17:38:09 +0200

    Remade the scene transition and stuff

--- Spaceman Hacktues/Assets/scripts/teleport.cs
+++ Spaceman Hacktues/Assets/scripts/teleport.cs
@@ -6,12 +6,22 @@ using UnityEngine.SceneManagement;
 public class teleport : cancolide
 {
     public string level;
+    public float time;
+    public Animator animator;
     protected override void Oncollide(Collider2D coll)
     {
         if(coll.name == "player")
         {
-            gamemanager.instance.SaveState();
-            SceneManager.LoadScene(level);
+            StartCoroutine(DoTransition());
         }
+    }
+    IEnumerator DoTransition()
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(time);
+
+        gamemanager.instance.SaveState();
+        SceneManager.LoadScene(level);
     }
 }
