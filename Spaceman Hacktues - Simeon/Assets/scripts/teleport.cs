212baa08836cd4f52404eef533f5da18a625bcb2
simeonikratko <bgjoje@gmail.com>
2022-03-12 05:27:06 +0200

    Worked on the next maze

---
+++ Spaceman Hacktues - Simeon/Assets/scripts/teleport.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleport : cancolide
+{
+    public string level;
+    protected override void Oncollide(Collider2D coll)
+    {
+        if(coll.name == "player")
+        {
+            gamemanager.instance.SaveState();
+            SceneManager.LoadScene(level);
+        }
+    }
+}
