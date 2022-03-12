a3696992b7cde00bea31788f2cfbd1d5a7e14414
Moiseicho 45 <mateizahariev@gmail.com>
2022-03-13 01:01:50 +0200

    Fixed local tilemap screwup and added first box puzzle

---
+++ Spaceman Hacktues/Assets/scripts/RemovewallsPButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemovewallsPButton : pushbutton
+{
+    public GameObject WalsDest;
+    protected override void Onpush()
+    {
+       WalsDest.SetActive(false);
+       Debug.Log("Shown");
+    }
+}
