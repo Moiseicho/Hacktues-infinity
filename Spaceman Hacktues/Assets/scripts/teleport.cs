f1a41d9fb1f56c0c12abfc2dd9652090162905cc
Moiseicho 45 <mateizahariev@gmail.com>
2022-03-12 16:10:10 +0200

    Added some details to the first labirinth

--- Spaceman Hacktues/Assets/scripts/teleport.cs
+++ Spaceman Hacktues/Assets/scripts/teleport.cs
@@ -6,12 +6,21 @@ using UnityEngine.SceneManagement;
 public class teleport : cancolide
 {
     public string level;
+    public Animator transition;
+    public float waittime;
     protected override void Oncollide(Collider2D coll)
     {
         if(coll.name == "player")
         {
-            gamemanager.instance.SaveState();
-            SceneManager.LoadScene(level);
+            StartCoroutine(WaitForTrans());
         }
+    }
+    IEnumerator WaitForTrans()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(waittime);
+
+        gamemanager.instance.SaveState();
+        SceneManager.LoadScene(level);
     }
 }
