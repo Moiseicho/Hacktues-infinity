7b6ea813bd18b34777df5a81717973da3d178151
Moiseicho 45 <mateizahariev@gmail.com>
2022-03-13 05:21:42 +0200

    Added ivans start screen and planet info scenes.

---
+++ Spaceman Hacktues/Assets/scripts/buttonscene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonscene : MonoBehaviour
+{
+    public string level;
+    public void MoveToScene()
+    {
+        SceneManager.LoadScene(level);
+    }
+}
